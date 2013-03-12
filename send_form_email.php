88e5c69131b16682c91cf15d21f2da4836bd3b0b
Brian Daly <dalynewspaper@gmail.com>
2013-03-12 21:43:00 +0000

    12/0/2013

--- send_form_email.php
+++ send_form_email.php
@@ -73,7 +73,7 @@ $headers = 'From: '.$email_from."\r\n".
 
 Thank you for contacting us. We will be in touch with you very soon.
 <script type="text/javascript">
-    window.location="http://pitchify.me/websiteapp"
+    window.location="http://westgrovefbapp.azurewebsites.net"
 </script>
 <?
 }
